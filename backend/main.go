361342a65c20538b67f1ec8297cb10a019ad13c8
Nicolas De Loof <nicolas.deloof@gmail.com>
2020-09-03 09:14:19 +0200

    Demo
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +29,55 @@ func handler(w http.ResponseWriter, r *http.Request) {
 Hello from Docker!
 
 `)
+
+	for _, pair := range os.Environ() {
+		fmt.Println(pair)
+	}
+
+	bucket := "jefaisuntest"
+	item := "/tmp/foo"
+	file, err := os.Create(item)
+
+	defer file.Close()
+
+	fmt.Println("Load AWS config...")
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Region: aws.String("eu-west-3"),
+		},
+		SharedConfigState: session.SharedConfigEnable,
+	})
+
+	if err != nil {
+		fmt.Sprintf("failed to load SDK config: %v", err)
+		return
+	}
+
+	fmt.Println("Create S3 Manager")
+	downloader := s3manager.NewDownloader(sess)
+
+	fmt.Println("Download file from s3")
+	numBytes, err := downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String("blabla.txt"),
+		})
+	if err != nil {
+		fmt.Fprintf(w, "Unable to download item %q, %v\n", item, err)
+		return
+	}
+
+	fmt.Fprintf(w, "Downloaded %s %d bytes", file.Name(), numBytes)
+
+	fmt.Fprintln(w)
+	fmt.Fprintln(w)
+
+	bytes, err := ioutil.ReadFile(item)
+	if err != nil {
+		fmt.Fprintf(w, "Unable to read downloaded item %q, %v\n", item, err)
+		return
+	}
+	w.Write(bytes)
 }
 
 func main() {
